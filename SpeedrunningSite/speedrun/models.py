98cdf4bb863f001c1115916ec1f38915a5e00d99
Massimo Nikolic <amnikoic@alaska.edu>
2023-12-10 06:15:02 -0900

    Completed User register, login, and logout

--- SpeedrunningSite/speedrun/models.py
+++ SpeedrunningSite/speedrun/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-from django.contrib.auth import AbstractUser
+from django.contrib.auth.models import AbstractUser
 
-# class User(AbstractUser): 
+class User(AbstractUser): 
+    pass
 
+# class Profile(models.Model):
+#     creator =  models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+#     profilePicture = models.ImageField()
